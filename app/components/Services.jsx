97966ee36bf8a2e49a420ae21cce6e4bb6bab6d2
Preet <preetjasani1708@gmail.com>
2025-02-10 19:54:12 +0530

    initial setup

---
+++ app/components/Services.jsx
@@ -0,0 +1,74 @@
+import { serviceData } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+import { motion } from "motion/react";
+
+const Services = () => {
+  return (
+    <motion.div
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h4
+        className="text-center mb-2 text-lg font-Ovo"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
+        What I offer
+      </motion.h4>
+      <motion.h2
+        className="text-center text-5xl font-Ovo"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        My Services
+      </motion.h2>
+      <motion.p
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+      >
+        I am a MERN stack developer from Ahmedabad, India with 7+ years of
+        experience across various types of projects.
+      </motion.p>
+      <motion.div
+        className="grid grid-cols-auto gap-6 my-10"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.9 }}
+      >
+        {serviceData.map(({ icon, title, description, link }, index) => (
+          <motion.div
+            key={index}
+            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
+            whileHover={{ scale: 1.05 }}
+          >
+            <Image src={icon} alt="service_icon" className="w-10" />
+            <h3 className="text-lg my-4 text-gray-700 dark:text-white">
+              {title}
+            </h3>
+            <p className="text-sm text-gray-600 leading-5 dark:text-white/80">
+              {description}
+            </p>
+            {/* <a href={link} className="flex items-center gap-2 text-sm mt-5">
+              Read more{" "}
+              <Image
+                src={assets.right_arrow}
+                alt="right_arrow_icon"
+                className="w-4"
+              />
+            </a> */}
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
